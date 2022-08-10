c1ee198d4a68dcfcc3d6e32658d7bbcb640633c3
Carlos E. Feria Vila <carlosthe19916@gmail.com>
2022-08-10 09:44:00 +0200

    Rebrand upload links to Windup (#1)

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
     ],
     windup: {
-      version: "5.3.0.Final",
+      version: "6.0.0.Final",
       idePlugins: [
         {
           title: "IntelliJ (Tech preview)",
