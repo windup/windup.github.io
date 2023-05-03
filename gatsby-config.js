846363b57cdea4d7c4a2aa5c0aa8dbf936cdefe5
mrizzi <mrizzi@redhat.com>
2023-05-03 15:08:36 +0200

    Windup 6.2.3.Final Release Notes

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
       },
     ],
     windup: {
-      version: "6.2.1.Final",
+      version: "6.2.3.Final",
       idePlugins: [
         // {
         //   title: "IntelliJ (Tech preview)",
